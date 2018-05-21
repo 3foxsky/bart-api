5e9abf2168960bc50a316f9c24923882146aea76
Matej Baco <meldiron@Matejs-MacBook-Pro.local>
2018-05-21 23:38:21 +0200

    Rest API /images

--- images.js
+++ images.js
@@ -1,14 +1,56 @@
+const jimp = require("jimp");
+const path = require("path");
 
 const settings = require('./settings.json');
 
 module.exports = (app) => {
     app.get('/images/:res/*', (req, res) => {
-        const resolution = req.params.res;
-        const path = req.url.split('/images/' + resolution + '/').join('');
+        const resString = req.params.res;
 
-        console.log(resolution);
-        console.log(path);
+        if(!resString || resString.indexOf("x") == -1) {
+            res.status(500).send('Nepodarilo sa spracovať obrázok a vygenerovať náhľad.');
+            return;
+        }
 
-        res.send("1");
+        const resolution = resString.split("x");
+
+        if(resolution.length != 2 || isNaN(resolution[0]) == true || isNaN(resolution[1]) == true || resolution[0] == "" || resolution[1] == "") {
+            res.status(500).send('Nepodarilo sa spracovať obrázok a vygenerovať náhľad.');
+            return;
+        }
+
+        resolution[0] = +resolution[0];
+        resolution[1] = +resolution[1];
+
+        const imagePath = req.url.split('/images/' + resString + '/').join('');
+
+        jimp.read(path.join(__dirname, settings.galleryFolder, imagePath), (err, image) => {
+            if(err) {
+                res.status(404).send('Obrázok sa nenašiel');
+                return;
+            }
+
+            if(resolution[0] != 0 && resolution[1] != 0) {
+                image.resize(resolution[0], resolution[1]);
+            } else if(resolution[0] == 0 && resolution[1] != 0) {
+                image.resize(jimp.AUTO, resolution[1]);
+            } else if(resolution[1] == 0 && resolution[0] != 0) {
+                image.resize(resolution[0], jimp.AUTO);
+            } else {
+                //nejaky extremista zadal rozlisenia 0x0
+                res.status(500).send('Nepodarilo sa spracovať obrázok a vygenerovať náhľad.');
+                return;
+            }
+
+            image.getBuffer(image._originalMime, (err, buffer) => {
+                if(err) {
+                    res.status(500).send('Nepodarilo sa spracovať obrázok a vygenerovať náhľad.');
+                    return;
+                }
+
+                res.set('Content-Type', image._originalMime);
+                res.send(buffer);
+            });
+        });
     });
 }
